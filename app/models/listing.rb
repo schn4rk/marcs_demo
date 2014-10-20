06f29666426d975c7b9a70813bc253cc04444290
Marc <schnark.beermann@googlemail.com>
2014-10-20 15:08:15 +0100

    renewal

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,8 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "/default.jpg",
-  						:storage => :dropbox,
-                        :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-                                        
-                        
+	Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.8.9-Q16'
+  	has_attached_file 					:image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg",
+  										:storage => :dropbox,
+    									:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  	validates_attachment_content_type 	:image, :content_type => /\Aimage\/.*\Z/
 end
 
